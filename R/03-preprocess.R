254096c396a0ce4a9d1865d526388bdc0980b58c
David <dhammond@economicsandpeace.org>
2020-03-08 11:52:32 +0000

    ecdf

--- R/03-preprocess.R
+++ R/03-preprocess.R
@@ -31,6 +31,17 @@ index_data_preprocess = function(df, index_meta_data_path = "index_meta_data/ind
     #this is a hack but it will do for now
     tmp$value[!as.logical(tmp$is_more_better)] = -tmp$value[!as.logical(tmp$is_more_better)]
     tmp = tmp %>% dplyr::left_join(index_data_summarise(tmp))
+    my_ecdf = stats::ecdf(tmp$knn_pc)
+    my_ecdf <- data.frame(
+      knn_threshold = tmp$knn_pc,
+      number_of_countries = floor(length(unique(tmp$geocode))*my_ecdf(tmp$knn_pc))
+    )
+    my_ecdf  = my_ecdf [order(my_ecdf$knn_threshold),]
+    my_ecdf = my_ecdf %>% dplyr::distinct()
+    plotly::ggplotly(ggplot2::ggplot(my_ecdf , ggplot2::aes(.data$knn_threshold, .data$number_of_countries)) + ggplot2::geom_line() +
+                       ggplot2::labs(title = "Number of knn imputations vs number of countries in index", x = "knn Threshold",
+                            y = "Number of Countries with less than knn threshold") +
+                       ggplot2::theme_minimal())
     return(tmp)
   }
 

c1fed54f299f252b345f12bc053e57308153bea5
David Hammond <David.Hammond@smartercapitalholdings.com.au.local>
2020-03-09 18:59:34 +1100

    rounding error

--- R/04-calculate.R
+++ R/04-calculate.R
@@ -32,7 +32,7 @@ index_calculate = function(df){
     dplyr::rename(variablename = .data$index_name) %>% dplyr::ungroup()
   scores = rbind(scores, domains)
   scores = scores %>% dplyr::group_by(.data$variablename, .data$year) %>%
-    dplyr::mutate(rank = rank(-.data$value), value = round(.data$value,2)) %>%
+    dplyr::mutate(rank = rank(-.data$value), value = round(.data$value,4)) %>%
     dplyr::ungroup()
   countryinfo = wbstats::wb_cachelist$countries %>% dplyr::rename(geocode = .data$iso3c) %>%
     dplyr::select(.data$geocode, .data$country,.data$region, .data$income)

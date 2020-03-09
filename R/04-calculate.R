05ac68ada4216478337b7c1bf051cee15d5f3425
David Hammond <David.Hammond@smartercapitalholdings.com.au.local>
2020-03-09 18:48:50 +1100

    wbstats

--- R/04-calculate.R
+++ R/04-calculate.R
@@ -31,6 +31,11 @@ index_calculate = function(df){
     dplyr::summarise(value = sum(.data$indicator_score)/sum(.data$weight)) %>%
     dplyr::rename(variablename = .data$index_name) %>% dplyr::ungroup()
   scores = rbind(scores, domains)
-  df = list(backend = df, scores = scores)
+  scores = scores %>% dplyr::group_by(.data$variablename, .data$year) %>%
+    dplyr::mutate(rank = rank(-.data$value), value = round(.data$value,2)) %>%
+    dplyr::ungroup()
+  countryinfo = wbstats::wb_cachelist$countries %>% dplyr::rename(geocode = .data$iso3c) %>%
+    dplyr::select(.data$geocode, .data$country,.data$region, .data$income)
+  df = list(backend = df, scores = scores, countryinfo = countryinfo)
   return(df)
 }

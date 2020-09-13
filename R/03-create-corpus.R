0a75321c51a7291a2b9f7b8eb19db5dd91c4ff08
david-hammond <anotherdavidhammond@gmail.com>
2020-09-13 20:49:53 +1000

    no ecdf

--- R/03-create-corpus.R
+++ R/03-create-corpus.R
@@ -26,17 +26,17 @@ index_create_data_corpus = function(df){
     tmp = tmp %>% dplyr::group_by(.data$geocode) %>%
       dplyr::mutate(knn_pc = sum(.data$imputation_type == "knn")/dplyr::n()) %>%
       dplyr::ungroup()
-    my_ecdf = stats::ecdf(tmp$knn_pc)
-    my_ecdf <- data.frame(
-      knn_threshold = tmp$knn_pc,
-      number_of_countries = floor(length(unique(tmp$geocode))*my_ecdf(tmp$knn_pc))
-    )
-    my_ecdf  = my_ecdf [order(my_ecdf$knn_threshold),]
-    my_ecdf = my_ecdf %>% dplyr::distinct()
-    p = plotly::ggplotly(ggplot2::ggplot(my_ecdf , ggplot2::aes(.data$knn_threshold, .data$number_of_countries)) + ggplot2::geom_line() +
-                       ggplot2::labs(title = "Number of knn imputations vs number of countries in index", x = "knn Threshold",
-                            y = "Number of Countries with less than knn threshold") +
-                       ggplot2::theme_minimal())
-    print(p)
+    # my_ecdf = stats::ecdf(tmp$knn_pc)
+    # my_ecdf <- data.frame(
+    #   knn_threshold = tmp$knn_pc,
+    #   number_of_countries = floor(length(unique(tmp$geocode))*my_ecdf(tmp$knn_pc))
+    # )
+    # my_ecdf  = my_ecdf [order(my_ecdf$knn_threshold),]
+    # my_ecdf = my_ecdf %>% dplyr::distinct()
+    # p = plotly::ggplotly(ggplot2::ggplot(my_ecdf , ggplot2::aes(.data$knn_threshold, .data$number_of_countries)) + ggplot2::geom_line() +
+    #                    ggplot2::labs(title = "Number of knn imputations vs number of countries in index", x = "knn Threshold",
+    #                         y = "Number of Countries with less than knn threshold") +
+    #                    ggplot2::theme_minimal())
+    # print(p)
     return(tmp)
 }

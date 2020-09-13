dfd54030716bdcc0bdf2ff72cc7351a2b34f75c0
david-hammond <anotherdavidhammond@gmail.com>
2020-09-13 20:41:16 +1000

    lowest

--- R/04-band.R
+++ R/04-band.R
@@ -12,8 +12,11 @@
 #'
 #' @author David Hammond
 #' @export
-index_data_band = function(values, worst_value = min(values), best_value = max(values)){
+index_data_band = function(df, worst_value = min(df$values), best_value = max(df$values)){
+  tmp = index_data_summarise(df)
   hist(values)
+  text(x = max(df$values), y = 0.5, tmp$highest_score, col = "green")
+  text(x = min(df$values), y = 0.1, tmp$lowest_score, col = "red")
   abline(v = worst_value, col = "red", cex = 2)
   abline(v = best_value, col = "green", cex = 2)
   text(x = best_value, y = 0.1, "BEST", col = "green")
